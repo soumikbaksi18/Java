40bd87541e885bc6425f3115ac29acf71d77161a
soumikbaksi18 <baksisoumik@gmail.com>
2024-01-29 14:02:37 +0530

    Assignment 1 done

---
+++ Date_Conversions/Assignment1/src/Assignment1.java
@@ -0,0 +1,35 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter date and time in the format dd/MM/yyyy HH:mm");
+        String userInput = scanner.nextLine();
+
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        try {
+            Date inputDate = inputFormat.parse(userInput);
+
+            System.out.println("Local timestamp:" + inputFormat.format(inputDate));
+
+            long unixTimestamp = inputDate.getTime() / 1000L;
+            System.out.println("Unix Timestamp:" + unixTimestamp);
+
+            SimpleDateFormat utcFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+            utcFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            String utcTimestamp = utcFormat.format(inputDate);
+            System.out.println("UTC Timestamp: " + utcTimestamp);
+
+            Date utcDate = utcFormat.parse(utcTimestamp);
+            System.out.println("Converted back to local timestamp :" + inputFormat.format(utcDate));
+        } catch (ParseException e) {
+            System.out.println("Invalid date format. Please enter the date in the specified format");
+        }
+    }
+}
--- Date_Conversions/Today_Date/src/SimpleDateFormatExample.java
+++ Date_Conversions/Today_Date/src/SimpleDateFormatExample.java
@@ -1,9 +1,10 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.*;
 
 public class SimpleDateFormatExample {
     public static void main(String args[]) {
-
+        Scanner in = new Scanner(System.in);
         Date date = new Date();
 
         String DATE_FORMAT = "dd/MM/yyyy";
