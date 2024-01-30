d061ebd4176b128dc1446a60c9c1cf842946d285
soumikbaksi18 <baksisoumik@gmail.com>
2024-01-30 18:26:46 +0530

    SpringBoot Basic API started

---
+++ SpringBoot_Assignment/employee_management/src/main/java/com/developer/employee_management/entity/EmployeeEntity.java
@@ -0,0 +1,78 @@
+package com.developer.employee_management.entity;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "mt_employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "dateOfBirth")
+    private int dateOfBirth;
+
+    @Column(name = "address")
+    private String address;
+
+    public EmployeeEntity(long id, String name, String gender, int dateOfBirth, String address) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+
+    public EmployeeEntity(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
